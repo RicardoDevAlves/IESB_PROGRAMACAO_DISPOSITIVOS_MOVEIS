3eb975ff808130c725c1339f2cb75acdcb64448e
RicardoDevAlves <motoric750@gmail.com>
2025-03-09 11:12:16 -0300

    resolvendo exercício 04...

---
+++ exercicios/exercicios-javascript/ex-04.js
@@ -0,0 +1,10 @@
+// Laços de Repetição
+// Crie um script que imprima os números de 1 a 100 usando:
+// 1. Um laço for .
+
+let contador = 1;
+let contagem = 100;
+
+for (;contador <= contagem; contador++){
+    console.log(contador)
+}
