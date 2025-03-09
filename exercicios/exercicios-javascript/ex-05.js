08510a209fb840898703de1a81ed5250df8e7b5d
RicardoDevAlves <motoric750@gmail.com>
2025-03-09 19:48:41 -0300

    resolvendo o ex-05.js

---
+++ exercicios/exercicios-javascript/ex-05.js
@@ -0,0 +1,24 @@
+// Funções
+// Crie um script que tenha uma função que receba um número e retorne:
+// 1. O dobro do número.
+// 2. O triplo do número.
+// 3. O quadrado do número.
+// Utilize o número 5 para os testes.
+
+function dobro(numero) {
+    let resultado = numero * 2
+    console.log("O dobro de "+numero+ " é "+resultado)
+}
+dobro(5)
+
+function triplo(numero) {
+    let resultado = numero * 3
+    console.log("O triplo de " +numero+ " é "+resultado)
+}
+triplo(5)
+
+function quadrado(numero) {
+    let resultado = numero ** 4
+    console.log("O quadrado de "+numero+" é "+resultado)
+}
+quadrado(5)
