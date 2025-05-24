93e84aede2a9624bbe7c4a2bbb280593c757fd0f
RicardoDevAlves <motoric750@gmail.com>
2025-05-24 18:16:44 -0300

    criando exercicio-loja...

---
+++ exercicios/exercicio-loja/src/routes/StackRoutes.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from '../screens/HomeScreen';
+import ProdutoScreen from '../screens/ProdutoScreen';
+import ListaProdutosScreen from '../screens/ListaProdutosScreen';
+
+const Stack = createStackNavigator();
+
+export default function StackRoutes() {
+  return (
+    <Stack.Navigator>
+        <Stack.Screen
+        name='HomeScreen'
+        component={HomeScreen} 
+        options={{
+            title: 'Página Inicial',
+            headerTitleAlign: 'center',
+        }}
+        />
+        <Stack.Screen
+          name='ProdutoScreen'
+          component={ProdutoScreen}
+          options={{
+            title: 'Produtos',
+            headerTitleAlign: 'center',
+          }}
+        />
+        <Stack.Screen 
+        name='ListaProdutosScreen'
+        component={ListaProdutosScreen}
+        options={{
+            title: 'Lista de Produtos',
+            headerTitleAlign: 'center',
+        }}
+        />
+    </Stack.Navigator>
+  )
+}
+
---
+++ exercicios/exercicio-loja/src/screens/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Text>Sucesso ao realizar essa tarefa devagar e aprendendo da forma correta!</Text>
+      <Text>Agora irei dar uma pausa para ir trabalhar na pizzaria.</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
